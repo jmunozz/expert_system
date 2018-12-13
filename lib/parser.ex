5db25f2fa858d3ba56c714fa267e483dc052df50
Jules BARBIER <jubarbie@e1r2p16.42.fr>
2018-12-13 16:41:37 +0100

    building tree

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,16 +1,39 @@
 defmodule Parser do
 
   def parse(str) do
-	  {a, b} = String.next_grapheme(str)
-	  parse(a, b, "")
+	  case String.next_grapheme(str) do
+		  {a, b} -> parse("", b, a)
+		  _ -> IO.puts "Error"
+	  end
   end
 
-  def parse(first, rest, current) when current in ["+", "^", "|"] do
- 	 [first, rest]
+  def parse(first, rest, current) when rest == "" do
+	  first <> current
   end
 
-  def parse(first, rest, current) when current in ["+", "^", "|"] do
- 	 [first, rest]
+  def parse(first, rest, current) when current == "(" do
+ 	 parse(first, rest, "", 0)
+  end
+
+  def parse(first, rest, current) when current in ["=", "<", "<="] do
+	 {a, b} = String.next_grapheme(rest)
+ 	 parse(first, b, current <> a)
+  end
+
+  def parse(first, rest, current) when current in ["<=>", "=>", "+", "^", "|"] do
+	  left =
+		if String.match?(first, ~r/^[a-z]$/i) do
+		  	Tree.leaf(first)
+		else
+			parse(first)
+		end
+	  right =
+		  if String.match?(rest, ~r/^[a-z]$/i) do
+			  Tree.leaf(rest)
+		  else
+			  parse(rest)
+		  end
+ 	  Tree.branch(current, left, right)
   end
 
   def parse(first, rest, current) do
@@ -18,6 +41,18 @@ defmodule Parser do
 	  parse(first <> current, b, a)
   end
 
-
+  def parse(first, rest, current, depth) do
+	  case String.next_grapheme(rest) do
+		  {"(", b} ->
+			  parse(first, b, current <> "(", depth + 1)
+     	  {")", b} ->
+			  	if depth == 0 do
+			  		parse(first, b, current)
+				else
+					parse(first, b, current <> ")", depth - 1)
+				end
+		  {a, b} -> parse(first, b, current <> a, depth)
+      end
+  end
 
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,7 +1,12 @@
 defmodule ParserTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Parser.parse("A+B+C") == ["A", "B+C"]
+  test "A+B" do
+	assert Parser.parse("A+B") == {:branch, "+", {:leaf, "A"}, {:leaf, "B"}}
   end
+
+  test "((A+B)|C)^B" do
+	assert Parser.parse("((A+B)|C)^B") == {:branch, "^", {:branch, "|", {:branch, "+", {:leaf, "A"}, {:leaf, "B"}}, {:leaf, "C"}}, {:leaf, "B"}}
+  end
+
 end
